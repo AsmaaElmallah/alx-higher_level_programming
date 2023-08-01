d31061db05782cb8e596bc1f19fa3b252d2cc65b
AsmaaElmallah <elmallah.ae@gmail.com>
2023-08-01 08:05:28 -0700

    l

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,9 +7,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result:", "{}".format(a))
     return a
-
-4
-#!/usr/bin/python3
-
-def list_division(my_list_1, my_list_2, list_length):
-
