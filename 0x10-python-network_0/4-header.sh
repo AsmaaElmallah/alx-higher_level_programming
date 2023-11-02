81a02f44945be9af9fd748a42f9c0a8ecf2dbfc4
AsmaaElmallah <elmallah.ae@gmail.com>
2023-11-02 07:47:32 -0700

    f

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to the URL using curl, and display the body of the response
-curl -sH "X-School-User-Id: 98" "$1"
+# URL as an argument, sends a GET request to the URL
+curl -s -H X-School-User-Id:98 ${1}
